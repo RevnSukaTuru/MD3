cd3e60dc52649a55cd89cb57d9c4acb70322547a
RevnSukaTuru <109857886+RevnSukaTuru@users.noreply.github.com>
2022-07-24 21:55:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-global.owner = ['6285748894171'] // Put your number here
+global.owner = ['6285760165634'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
-  // name: 'https://website'
+  name: 'https://api.zeeoneofc.xyz'
   amel: 'https://melcanz.com',
     bx: 'https://bx-hunter.herokuapp.com',
   dhnjing: 'https://dhnjing.xyz',
@@ -26,7 +26,7 @@ global.APIs = { // API Prefix
   LeysCoder: 'https://leyscoders-api.herokuapp.com'
 }
 global.APIKeys = { // APIKey Here
-  // 'https://website': 'apikey'
+  'https://api.zeeoneofc.xyz': 'apikey'
   'https://melcanz.com': 'elaina',
   'https://api.xteam.xyz': 'd37372311698ed1d',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
@@ -42,12 +42,12 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
-global.packname = 'stickerby'
-global.author = `NakataArdnt`
-global.wm = 'NahBotz-' //Nama Bot
+global.packname = 'BY V BOTZ'
+global.author = `VanzzM`
+global.wm = 'VBOTZ-' //Nama Bot
 global.watermark = wm
 global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
-global.instagram = 'https://instagram.com/nakataa1.a'
+global.instagram = 'https://instagram.com/vanzzxyt_official'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&doScale=true&scaleWidth=500&scaleHeight=500&fontsize=100&fillTextType=0&backgroundColor=%23401620&text='
 global.wait = '_*tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
